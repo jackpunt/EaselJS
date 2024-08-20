2d0e45c964c969c4b44ebdc3a45ceec6aaa71b75
jackpunt <jpeckj@gmail.com>
2024-08-20 16:15:03 -0700

    use array.indexOf for createjs.indexOf

--- src/createjs/utils/indexOf.js
+++ src/createjs/utils/indexOf.js
@@ -50,11 +50,11 @@ this.createjs = this.createjs||{};
  */
 createjs.indexOf = function (array, searchElement){
 	"use strict";
-
-	for (var i = 0,l=array.length; i < l; i++) {
-		if (searchElement === array[i]) {
-			return i;
-		}
-	}
-	return -1;
+	return array.indexOf(searchElement);
+	// for (var i = 0,l=array.length; i < l; i++) {
+	// 	if (searchElement === array[i]) {
+	// 		return i;
+	// 	}
+	// }
+	// return -1;
 };
